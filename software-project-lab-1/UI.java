38ddac8f85e8a6101ef05e3cfaed288c0250198e
Redowan Ahmed <161846618+redowannahmed@users.noreply.github.com>
2024-11-05 10:57:44 -0800

    Add files via upload
    refactored files

---
+++ software-project-lab-1/UI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UI {
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("Error clearing screen: " + e.getMessage());
+        }
+    }
+
+    public static void waitForUser(Scanner sc) {
+        System.out.println("Press Enter to continue...");
+        sc.nextLine();
+    }
+}
