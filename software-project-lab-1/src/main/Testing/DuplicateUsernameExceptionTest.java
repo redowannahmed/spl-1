fd682fcd4d6aed3c3815b0065d1654bb5fd8039b
SamiunAuntor <samiunalimauntor@gmail.com>
2025-03-18 17:26:26 +0600

    Updated DuplicateUsernameExceptionTest.java

--- software-project-lab-1/src/main/Testing/DuplicateUsernameExceptionTest.java
+++ software-project-lab-1/src/main/Testing/DuplicateUsernameExceptionTest.java
@@ -1,2 +1,16 @@
-package Testing;public class DuplicateUsernameExceptionTest {
+package Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import exceptions.DuplicateUsernameException;
+import org.junit.jupiter.api.Test;
+
+public class DuplicateUsernameExceptionTest {
+    @Test
+    public void testExceptionMessage() {
+        Exception exception = assertThrows(DuplicateUsernameException.class, () -> {
+            throw new DuplicateUsernameException("Duplicate Username found");
+        });
+        assertEquals("Duplicate Username found", exception.getMessage());
+    }
 }
